9632efb38ea3cf2ae7d17afb5b61ba175b6187a9
dorahei <dorahei_mail@dorahei.com>
2020-06-06 14:19:36 +0900

    ファイルの復元操作

---
+++ gatsby-config.js
@@ -0,0 +1,42 @@
+/**
+ * Configure your Gatsby site with this file.
+ *
+ * See: https://www.gatsbyjs.org/docs/gatsby-config/
+ */
+
+module.exports = {
+  /* Your site config here */
+  siteMetadata: {
+    title: `一克`,
+    description: ` 奥越の美味しい炭火焼き、串揚げ、串焼きのお店`,
+    lang: `ja`,
+    siteUrl: `https://katsuyama-ikkatsu.netlify.app`,
+    locale: `ja_JP`,
+    fbappid: `XXXXXXXXXXXXXXXXXXXXX`,
+  },
+  plugins: [
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images/`,
+      },
+    },
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `~炭火焼き~ 一克`,
+        short_name: `一克`,
+        start_url: `/`,
+        background_color: `#333333`,
+        theme_color: `colar`,
+        display: `standalone`,
+        icon: `src/images/icon.png`,
+      },
+    },
+    `gatsby-plugin-offline`,
+  ],
+}

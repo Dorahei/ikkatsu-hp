0a1578449f4c9fa4f06fb178201a87b6c0107e74
dorahei <dorahei_mail@dorahei.com>
2022-05-30 21:37:07 +0900

    ステージング後、再コミット

---
+++ gatsby-config.js
@@ -0,0 +1,44 @@
+/**
+ * Configure your Gatsby site with this file.
+ *
+ * See: https://www.gatsbyjs.org/docs/gatsby-config/
+ */
+
+module.exports = {
+  /* Your site config here */
+  siteMetadata: {
+    title: `-炭火串焼き串揚げ- 一克`,
+    description: ` 奥越、勝山市の美味しい炭火焼き、串揚げ、串焼きの居酒屋。国産炭火と国産鶏を使った美味しい焼鳥。`,
+    lang: `ja`,
+    siteUrl: `https://yakitori-ikkatsu.com`,
+    locale: `ja_JP`,
+    fbappid: `XXXXXXXXXXXXXXXXXXXXX`,
+  },
+  plugins: [
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images/`,
+      },
+    },
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `-炭火串焼き串揚げ- 一克`,
+        short_name: `一克`,
+        start_url: `/`,
+        background_color: `#fff`,
+        theme_color: `colar`,
+        display: `standalone`,
+        icon: `src/images/ikkatsu_icon.png`,
+      },
+    },
+    `gatsby-plugin-offline`,
+    `gatsby-plugin-sitemap`,
+    "gatsby-plugin-robots-txt",
+  ],
+}
---
+++ src/pages/about.js
@@ -0,0 +1,110 @@
+import React from "react"
+import { graphql } from "gatsby"
+import Img from "gatsby-image"
+
+import Layout from "../components/layout"
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faUtensils, faCheckSquare } from "@fortawesome/free-solid-svg-icons"
+
+import SEO from "../components/seo"
+
+export default ({ data, location }) => (
+  <Layout>
+    <SEO
+      pagetitle="一克 について"
+      pagedesc="勝山の炭火 串焼き 串揚げのお店"
+      pagepath={location.pathname}
+      pageimg={data.about.childImageSharp.original.src}
+      pageimgw={data.about.childImageSharp.original.width}
+      pageimgh={data.about.childImageSharp.original.height}
+    />
+    <div className="eyecatch">
+      <figure>
+        <Img
+          fluid={data.about.childImageSharp.fluid}
+          alt="一克店舗"
+        />
+      </figure>
+    </div>
+    <article className="content">
+      <div className="container">
+        <h1 className="bar">一克について</h1>
+        <aside className="info">
+          <div className="subtitle">
+            <FontAwesomeIcon icon={faUtensils} />
+            ABOUT IKKATSU
+          </div>
+        </aside>
+        <div className="postbody">
+          <p>
+            美しさと美味しさが一克(括)。<br /><br />
+            お店に着くとまず食欲をそそる焼き鳥の香ばしい匂いがお出迎え<br />
+            店内はカウンター席と小上がりの座敷があります。<br /><br />
+            落ち着いた雰囲気でお食事を楽しめます。<br />
+            カウンターの目の前で炭火で焼く串焼きは見て食べて２度美味しいです。<br /><br />
+            お一人様でも大人数でもＯＫ！！<br /><br />
+            特に女子会でのご利用は大好評！<br />
+            いろいろなメニューの中でも野菜巻きはヘルシー感が受けて女性に人気
+          </p>
+          <h2>
+            <FontAwesomeIcon icon={faCheckSquare} />
+            店舗について
+          </h2>
+          <ul>
+            <li>カウンター席 9席</li>
+            <li>テーブル 12～14席</li>
+            <li>トイレ 男女別</li>
+          </ul>
+          <h2>
+            <FontAwesomeIcon icon={faCheckSquare} />
+            よく聞かれること
+          </h2>
+          <p>
+            駐車場に関して
+          </p>
+          <ul>
+            <li>第一駐車場　2台</li>
+            <li>第二駐車場　4台</li>
+            <li>満車の場合は勝山中央駐車場をご利用ください。
+            </li>
+          </ul>
+          <h2>
+            <FontAwesomeIcon icon={faCheckSquare} />
+            定休日
+          </h2>
+          <div className="biz-time">
+          <p>
+            <span>日曜日</span>
+            <br/>※翌日祝日の場合営業
+            <br/>（月曜日祝日の場合は月曜日休み）
+          </p>
+          <hr/>
+          <p>
+            <span>営業時間</span>　17：00 ~ 23：00 
+            <br/>（ラストオーダー 22：00）
+          </p>
+          <p>気長にお付き合いいただければ幸いです。</p>
+          </div>
+        </div>
+      </div>
+    </article>
+  </Layout>
+)
+
+export const query = graphql`
+  query {
+    about: file(relativePath: { eq: "shop_image.jpg" }) {
+      childImageSharp {
+        fluid(maxWidth: 1600) {
+          ...GatsbyImageSharpFluid_withWebp
+        }
+        original {
+          src
+          height
+          width
+        }
+      }
+    }
+  }
+`
